0071026bb796857a39dc9478ab7364f14f6e3db4
Dev Kaliper <gxlthz@gmail.com>
2023-07-23 18:38:00 -0500

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => {
 <footer className="footer bg-[#988E6F] glass absolute bottom-0 items-center p-4  text-white">
   <div className="items-center grid-flow-col">
     
-    <p>DevKaliper © 2023 - All right reserved</p>
+    <p>Manuel Leon © 2023 - All right reserved</p>
   </div> 
   <div className=" md:place-self-center md:justify-self-end">
     <a href='https://github.com/DevKaliper' rel='noreferrer' target='_blank' className='flex gap-2 justify-center items-center text-lg font-mono'> <img src="./src/img/github-mark.png" className='invert' width={50} alt="" /> my Github
