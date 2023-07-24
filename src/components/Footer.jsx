5ba19fcb308fbf3fcb6485c9bfe9106639916917
Dev Kaliper <gxlthz@gmail.com>
2023-07-23 19:09:08 -0500

    fixing images bugs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export const Footer = () => {
     <p>Manuel Leon © 2023 - All right reserved</p>
   </div> 
   <div className=" md:place-self-center md:justify-self-end">
-    <a href='https://github.com/DevKaliper' rel='noreferrer' target='_blank' className='flex gap-2 justify-center items-center text-lg font-mono'> <img src="./src/img/github-mark.png" className='invert' width={50} alt="" /> my Github
+    <a href='https://github.com/DevKaliper' rel='noreferrer' target='_blank' className='flex gap-2 justify-center items-center text-lg font-mono'> <img src="https://cdn.discordapp.com/attachments/1125465851810820142/1132826120392286218/github-mark-white.png"  width={50} alt="" /> my Github
     </a> 
    
   </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,13 @@ const Header = () => {
         <div className="navbar glass gap-10 px-32 bg-[#988E6F]">
   <a href="https://github.com/" target="_blank" rel="noreferrer" className="flex-1  btn btn-ghost normal-case text-xl relative ">
     <a className="invisible md:visible">Intergrated with </a>
-    <img src="./src/img/GitHub_Logo_White.png"  className="absolute md:relative" alt="github" width={150} />
+    <img src="https://cdn.discordapp.com/attachments/1125465851810820142/1132826187685707896/GitHub_Logo_White.png"  className="absolute md:relative" alt="github" width={150} />
   </a>
   <div className="flex-none">
     <a href="https://github.com/DevKaliper/github-find-users" target="_blank" rel="noreferrer" className="btn btn-square btn-ghost flex w-fit h-fit gap-3 justify-center items-center">
-         <img src="./src/img/github-mark.png" alt="" width={50} />
+         <img src="https://cdn.discordapp.com/attachments/1125465851810820142/1132826120392286218/github-mark-white.png" alt="" width={50} />
         
-    <span className="font-mono text-base text-black underline invisible md:visible"> repo here</span>
+    <span className="font-mono text-base text-white underline invisible md:visible"> repo here</span>
     </a>
 
   </div>
